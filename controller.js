44dc00159c05300fc5ade445edd98ae83a02d578
OwenRS99 <48766637+OwenRS99@users.noreply.github.com>
2022-10-05 14:21:20 +0100

    Game logic updated with tie/win & draw conditions

--- controller.js
+++ controller.js
@@ -12,20 +12,44 @@ function getComputerChoice()
 }
 
 function getPlayerChoice() {
-	let choice = prompt("Choose either Rock, Paper or Scissors")
-	
+	let choice = prompt("Choose either Rock, Paper or Scissors").toUpperCase();
 	if (choice == "paper") {
-		return 1
+		return 1;
 	} else if (choice == "rock") {
-		return 2
+		return 2;
 	} else {
-		return 3
+		return 3;
 	}
 }
 
 
 function playRound(getPlayerChoice, getComputerChoice)
+{	
+		let paper = 1;
+		let rock = 2;
+		let scissors = 3;
+		if (getPlayerChoice == getComputerChoice){
+			return console.log("Tie");
+		} else if (getPlayerChoice == 1 && getComputerChoice == 2 || getPlayerChoice == 2 && getComputerChoice == 3
+				   || getPlayerChoice == 3 && getComputerChoice == 1){
+			return console.log("Player Wins")
+		} else {
+			return console.log("Computer Wins");
+		}
+		
+}	
+
+function game()
 {
-	
+	for(let i = 0; i < 5; i++) {
+		if (getPlayerChoice())
+		{
+			playRound(getPlayerChoice, getComputerChoice);
+			
+			
+		}
+	}
 }
 
+
+game();
