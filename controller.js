4df253f8549358c08b3953edd5e792d81cf77a58
OwenRS99 <48766637+OwenRS99@users.noreply.github.com>
2022-10-05 16:06:15 +0100

    Updated game logic, changed return to strings

--- controller.js
+++ controller.js
@@ -2,54 +2,61 @@ function getComputerChoice()
 {
 	let choice = Math.floor(Math.random() * 3);
 	
-	if (choice < 1) {
-		return ("paper");
+	if (choice == 1) {
+		return ("PAPER");
 	} else if (choice == 2) {
-		return ("rock");
+		return ("ROCK");
 	} else {
-		return ("scissors");
+		return ("SCISSORS");
 	}
 }
 
 function getPlayerChoice() {
 	let choice = prompt("Choose either Rock, Paper or Scissors").toUpperCase();
-	if (choice == "paper") {
-		return 1;
-	} else if (choice == "rock") {
-		return 2;
-	} else {
-		return 3;
+	
+	if (choice == "PAPER") {
+		return "PAPER";
+		
+	} else if (choice == "ROCK") {
+		return "ROCK";
+		
+	} else if (choice == "SCISSORS"){
+		return "SCISSORS";
+		
+	}
+	
+	if (choice != "PAPER" || "SCISSORS" || "ROCK")
+	{
+		console.log("Wrong Entry");
+		choice;
 	}
 }
-
 
 function playRound(getPlayerChoice, getComputerChoice)
 {	
-		let paper = 1;
-		let rock = 2;
-		let scissors = 3;
 		if (getPlayerChoice == getComputerChoice){
 			return console.log("Tie");
-		} else if (getPlayerChoice == 1 && getComputerChoice == 2 || getPlayerChoice == 2 && getComputerChoice == 3
-				   || getPlayerChoice == 3 && getComputerChoice == 1){
+		}	
+	    else if (getPlayerChoice == "PAPER" && getComputerChoice == "ROCK" || getPlayerChoice == "ROCK" && getComputerChoice == "SCISSORS"
+				   || getPlayerChoice == "SCISSORS" && getComputerChoice == "PAPER"){
 			return console.log("Player Wins")
+			
 		} else {
 			return console.log("Computer Wins");
-		}
-		
+		}	
 }	
+
+let playerChoice = getPlayerChoice();
+let computerChoice = getComputerChoice();
 
 function game()
 {
-	for(let i = 0; i < 5; i++) {
-		if (getPlayerChoice())
-		{
-			playRound(getPlayerChoice, getComputerChoice);
-			
-			
-		}
+	for (let i = 0; i < 5; i++) 
+	{
+		playRound(playerChoice, computerChoice);
+		console.log("Player:", playerChoice);
+		console.log("Computer", computerChoice);
 	}
 }
-
 
 game();
